e2ca8b9b37ba6b61eb5029c9ed53f3fb12476248
Dixy-05 <ddbarahona89@gmail.com>
2020-08-04 18:29:16 -0600

    fixed date order, ask before reset list and budget

--- quickBudget.js
+++ quickBudget.js
@@ -50,7 +50,7 @@ function createItem() {
   checkBox.name = 'checkBoxName';
   checkBox.type = 'checkbox';
   item.appendChild(checkBox);
-  //Add event to item Checkbox-----------------------------//
+  //Add event(cross through) to item's Checkbox-----------------------------//
   var theCheckBox = document.getElementsByClassName('checkBoxClass');
   function crossThrough() {
     for (i = 0; i < theCheckBox.length; i++) {
@@ -73,15 +73,42 @@ function createItem() {
 
 // Adding event to  "Reset list" button-------------------------------------//
 var newListButton = document.getElementById('nListButton');
-newListButton.addEventListener('click', createNewList);
-function createNewList() {
-  document.getElementById('shoppingList').innerHTML = '';
-  /*var items = document.getElementsByClassName('itemClass');
-  var itemsLength = items.length;
-  for (i = 0; i < itemsLength; i++) {
-    var item = items[0];
-    document.getElementById('shoppingList').removeChild(item);
-  }*/
+newListButton.addEventListener('click', askBeforeNewList);
+function askBeforeNewList() {
+  if (document.getElementById('askResetList').innerHTML == '') {
+    var askingNewList = document.createElement('span');
+    askingNewList.id = 'askingNewList';
+    askingNewList.innerHTML = 'Are you sure you want to create  new list?';
+    document.getElementById('askResetList').appendChild(askingNewList);
+    var yesButtonList = document.createElement('button');
+    yesButtonList.id = 'yesButtonList';
+    var yesNode_list = document.createTextNode('Yes');
+    yesButtonList.appendChild(yesNode_list);
+    var noButtonList = document.createElement('button');
+    noButtonList.id = 'noButtonList';
+    var noNode_list = document.createTextNode('No');
+    noButtonList.appendChild(noNode_list);
+    document.getElementById('answerResetList').appendChild(yesButtonList);
+    document.getElementById('answerResetList').appendChild(noButtonList);
+    yesButtonList.addEventListener('click', createNewList);
+    function createNewList() {
+      document.getElementById('shoppingList').innerHTML = '';
+      /*var items = document.getElementsByClassName('itemClass');
+    var itemsLength = items.length;
+    for (i = 0; i < itemsLength; i++) {
+      var item = items[0];
+      document.getElementById('shoppingList').removeChild(item);
+    }*/
+      document.getElementById('askResetList').innerHTML = '';
+      document.getElementById('answerResetList').innerHTML = '';
+    }
+  } else {
+  }
+  noButtonList.addEventListener('click', stopNewList);
+  function stopNewList() {
+    document.getElementById('askResetList').innerHTML = '';
+    document.getElementById('answerResetList').innerHTML = '';
+  }
 }
 
 //container fase 1
@@ -119,11 +146,12 @@ function calculate() {
 
   var currentDate = new Date();
   var formatedDate =
-    currentDate.getFullYear() +
-    '-' +
-    (currentDate.getMonth() + 1) +
+    currentDate.getMonth() +
+    1 +
     '-' +
     currentDate.getDate() +
+    '-' +
+    currentDate.getFullYear() +
     '  ' +
     '/' +
     ' ' +
@@ -139,16 +167,46 @@ function calculate() {
 }
 //Reseting the budget-----------------------------------------------------//
 var newBudget = document.getElementById('createNew');
-newBudget.addEventListener('click', createNewBudget);
-function createNewBudget() {
-  var monthInput = document.getElementById('amount');
-  monthInput.value = '';
-  var weekBudget = document.getElementById('wcalcu');
-  weekBudget.innerHTML = '0.00';
-  document.getElementById('shoppingList').innerHTML = '';
-  document.getElementById('noteInput').value = '';
-  document.getElementById('weekBalance').innerHTML = '0.00';
-  document.getElementById('monthBalance').innerHTML = ' 0.00';
-  var classLogList = document.getElementById('logs');
-  classLogList.innerHTML = '';
+newBudget.addEventListener('click', askResetBudget);
+function askResetBudget() {
+  if (document.getElementById('askingResetButton').innerHTML == '') {
+    var noButton = document.createElement('button');
+    noButton.id = 'noButton';
+    var noTextNode = document.createTextNode('No');
+    noButton.appendChild(noTextNode);
+    var yesButton = document.createElement('button');
+    yesButton.id = 'yesButton';
+    var yesTextNode = document.createTextNode('Yes');
+    yesButton.appendChild(yesTextNode);
+    var ask2 = document.createElement('span');
+    ask2.id = 'ask2';
+    ask2.innerHTML = 'Are you sure you want to reset budget?';
+    document.getElementById('askingResetButton').appendChild(ask2);
+    document.getElementById('answerResetButton').appendChild(yesButton);
+    document.getElementById('answerResetButton').appendChild(noButton);
+    yesButton.addEventListener('click', resetBudget);
+    function resetBudget() {
+      var monthInput = document.getElementById('amount');
+      monthInput.value = '';
+      var weekBudget = document.getElementById('wcalcu');
+      weekBudget.innerHTML = '0.00';
+      document.getElementById('shoppingList').innerHTML = '';
+      document.getElementById('noteInput').value = '';
+      document.getElementById('purchasein').value = '';
+      document.getElementById('weekBalance').innerHTML = '0.00';
+      document.getElementById('monthBalance').innerHTML = ' 0.00';
+      var classLogList = document.getElementById('logs');
+      classLogList.innerHTML = '';
+      document.getElementById('askingResetButton').innerHTML = '';
+      document.getElementById('answerResetButton').innerHTML = '';
+    }
+  } else {
+  }
+  noButton.addEventListener('click', noChangeB);
+  function noChangeB() {
+    document.getElementById('askingResetButton').innerHTML = '';
+    document.getElementById('answerResetButton').innerHTML = '';
+  }
 }
+
+//Ask before reseting budget --------------------------------------//
